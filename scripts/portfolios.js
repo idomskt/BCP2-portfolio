1bfd724863321f3d7bb42edd825985168de8f1c3
idomskt <idomskt@gmail.com>
2018-10-25 20:58:24 -0700

    the form is ready to add new portfolios

--- scripts/portfolios.js
+++ scripts/portfolios.js
@@ -21,6 +21,7 @@ Portfolio.allPortfolios = function(rawData) {
 Portfolio.printToScreen = function() {
     Portfolio.all.forEach(function (eachPortfolio) {
         $('#projectsList').append(eachPortfolio.toHtml());
+        
     });
 }
 
